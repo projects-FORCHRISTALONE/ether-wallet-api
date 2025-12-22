d7f1ca0b74ee8ef224ca81504c12f3817ecada7f
unworthyslaveTOCHRIST <righteousnessbyfaithinjesus@gmail.com>
2025-12-22 14:34:34 +0100

    Gracious first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+// ALL THANKS AND GLORY TO THE AND my ONLY GOD AND LORD JESUS CHRIST ALONE
+// BY GOD'S GRACE ALONE
+
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+
+app.set("port", process.env.PORT || 001)
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req,res)=>{
+    // res.type("application/json")
+    res.status(200).json({praiseToTheLord: "The Lord alone is praised"})
+})
+
+app.post("/api/contracts", (req,res,next)=>{
+
+    if(!req.body || Object.keys(req.body).length === 0){
+        return res.status(400).json({error: "No data provided"})
+    }
+
+    const payload = req.body;
+
+    const filePath = path.join(__dirname, "contract_registry.json");
+
+    let registry = [];
+
+    if (fs.existsSync(filePath)){
+        registry = JSON.parse(fs.readFileSync(filePath))
+    }
+
+    registry.push(payload);
+
+    fs.writeFileSync(filePath, JSON.stringify(registry, null, 2))
+
+    // res.type("application/json");
+
+    res.status(200).json({appended: true})
+})
+
+app.get("/api/retrieve_contract_details", (req,res)=>{
+
+    filePath = path.join(__dirname, "contract_registry.json");
+
+    if (fs.existsSync(filePath)){
+        registry = JSON.parse(fs.readFileSync(filePath))
+        res.status(200).json({data: registry})
+    }
+    else{
+        res.status(400).json({error: "No data provided"})
+    }
+})
+
+app.listen(app.get("port"), ()=>{
+    console.log(`Server graciously running on port ${app.get("port")}`)
+})
