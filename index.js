6ce0db66879111defffda256ea06798f8b2fab50
projects-FORCHRISTALONE <orimadegunemma@gmail.com>
2025-12-31 13:36:12 +0100

    Graciously added new origin to cors allowedOrigins list

--- index.js
+++ index.js
@@ -12,19 +12,19 @@ const app = express();
 
 const allowedOrigins = [
     "http://localhost:5173",
-    "https://ether-wallet-frontend.vercel.app/"
+    "https://ether-wallet-frontend.vercel.app"
 ]
 
 app.use(cors({
-    // origin: (origin, callback)=>{
-    //     if (!origin) return callback(null, true);
-    //     if (allowedOrigins.includes(origin)){
-    //         callback(null, true)
-    //     } else {
-    //         callback( new Error ("Not allowed by CORS"))
-    //     }
-    // },
-    origin: "https://ether-wallet-frontend.vercel.app",
+    origin: (origin, callback)=>{
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.includes(origin)){
+            callback(null, true)
+        } else {
+            callback( new Error ("Not allowed by CORS"))
+        }
+    },
+    // origin: "https://ether-wallet-frontend.vercel.app",
     methods: ["GET", "POST", "OPTIONS"],
     allowedHeaders: ["Content-Type"],
     credentials: false
